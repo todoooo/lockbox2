60d0aa141f6ad11cc9a2b73d9a777ffb22d29d29
toddm <todoooo@users.noreply.github.com>
2019-01-14 14:30:02 +1300

    Allow RSA keys to be loaded for signature verification

--- examples/Delphi/RSASSA1.pas
+++ examples/Delphi/RSASSA1.pas
@@ -32,19 +32,24 @@ type
   TForm1 = class(TForm)
     LbRSASSA1: TLbRSASSA;
     StatusBar1: TStatusBar;
-    Label1: TLabel;
-    mmoSignature: TMemo;
     GroupBox1: TGroupBox;
     Label2: TLabel;
     cbxKeySize: TComboBox;
     btnGenKeys: TButton;
     Label4: TLabel;
-    edtMsg: TEdit;
     btnSign: TButton;
     btnVerify: TButton;
     Label3: TLabel;
     cbxHashMethod: TComboBox;
     chkAbort: TCheckBox;
+    btnLoadPublic: TButton;
+    OpenDialog1: TOpenDialog;
+    btnLoadPrivate: TButton;
+    GroupBox2: TGroupBox;
+    mmoSignature: TMemo;
+    rbnHexidecimal: TRadioButton;
+    rbnBase64: TRadioButton;
+    edtMsg: TMemo;
     procedure btnSignClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure btnGenKeysClick(Sender: TObject);
@@ -54,8 +59,12 @@ type
     procedure LbRSASSA1GetSignature(Sender: TObject;
       var Sig: TRSASignatureBlock);
     procedure LbRSASSA1Progress(Sender: TObject; var Abort: Boolean);
+    procedure btnLoadPublicClick(Sender: TObject);
+    procedure btnLoadPrivateClick(Sender: TObject);
+    procedure rbnHexidecimalClick(Sender: TObject);
+    procedure rbnBase64Click(Sender: TObject);
   private
-    { Private declarations }
+
   public
     { Public declarations }
   end;
@@ -68,7 +77,7 @@ implementation
 {$R *.dfm}
 
 uses
-  LbUtils;
+  LbUtils, LbBigInt;
 
 const
   sPass = ' Signature verification: PASSED';
@@ -100,14 +109,67 @@ begin
   end;
 end;
 
+procedure TForm1.btnLoadPrivateClick(Sender: TObject);
+var
+  FS : TFileStream;
+begin
+  if OpenDialog1.Execute then begin
+    FS := TFileStream.Create(OpenDialog1.FileName, fmOpenRead);
+    Screen.Cursor := crHourGlass;
+    try
+      LbRSASSA1.PrivateKey.LoadFromStream(FS);
+    finally
+      FS.Free;
+      Screen.Cursor := crDefault;
+    end;
+  end;
+end;
+
+procedure TForm1.btnLoadPublicClick(Sender: TObject);
+var
+  FS : TFileStream;
+begin
+  if OpenDialog1.Execute then begin
+    FS := TFileStream.Create(OpenDialog1.FileName, fmOpenRead);
+    Screen.Cursor := crHourGlass;
+    try
+      LbRSASSA1.PublicKey.LoadFromStream(FS);
+    finally
+      FS.Free;
+      Screen.Cursor := crDefault;
+    end;
+  end;
+end;
+
 procedure TForm1.btnSignClick(Sender: TObject);
   { sign message string, display signature as hex string }
+var
+  SignatureCopy : TLbBigInt;
 begin
   Screen.Cursor := crHourglass;
   StatusBar1.SimpleText := sSigning;
   try
     LbRSASSA1.SignString(StringToUTF8(edtMsg.Text));
-    mmoSignature.Text := LbRSASSA1.Signature.IntStr;
+
+    SignatureCopy := TLbBigInt.Create(LbRSASSA1.Signature.Size);
+    try
+      SignatureCopy.Copy(LbRSASSA1.Signature);
+      SignatureCopy.PowerAndMod(LbRSASSA1.PublicKey.Exponent,LbRSASSA1.PublicKey.Modulus);
+      SignatureCopy.ReverseBytes;
+    finally
+      SignatureCopy.Free;
+    end;
+
+    if rbnBase64.Checked then
+    begin
+      mmoSignature.Text := LbRSASSA1.Signature.Base64Str;
+    end
+    else
+    begin
+      mmoSignature.Text := LbRSASSA1.Signature.IntStr;
+    end;
+
+    mmoSignature.Font.Color := clRed;
   finally
     Screen.Cursor := crDefault;
     StatusBar1.SimpleText := '';
@@ -117,17 +179,27 @@ end;
 procedure TForm1.btnVerifyClick(Sender: TObject);
   { verify signature against message }
 begin
+  StatusBar1.SimpleText := sFail;
   if LbRSASSA1.VerifyString(StringToUTF8(edtMsg.Text)) then
-    StatusBar1.SimpleText := sPass
-  else
-    StatusBar1.SimpleText := sFail;
+  begin
+    StatusBar1.SimpleText := sPass;
+    mmoSignature.Font.Color := clGreen;
+  end;
 end;
 
-procedure TForm1.LbRSASSA1GetSignature(Sender: TObject;
-  var Sig: TRSASignatureBlock);
+procedure TForm1.LbRSASSA1GetSignature(Sender: TObject; var Sig: TRSASignatureBlock);
   { convert signature string to binary and return it }
+var
+  BufferSize : Cardinal;
 begin
-  HexToBuffer(mmoSignature.Text, Sig, SizeOf(Sig));
+  if rbnBase64.Checked then
+  begin
+    Base64ToBuffer(mmoSignature.Text, Sig, BufferSize);
+  end
+  else
+  begin
+    HexToBuffer(mmoSignature.Text, Sig, SizeOf(Sig));
+  end;
 end;
 
 procedure TForm1.cbxKeySizeChange(Sender: TObject);
@@ -147,6 +219,18 @@ procedure TForm1.LbRSASSA1Progress(Sender: TObject; var Abort: Boolean);
 begin
   Application.ProcessMessages;
   Abort := chkAbort.Checked;
+end;
+
+procedure TForm1.rbnBase64Click(Sender: TObject);
+begin
+  rbnHexidecimal.Checked := False;
+  mmoSignature.Text := LbRSASSA1.Signature.Base64Str;
+end;
+
+procedure TForm1.rbnHexidecimalClick(Sender: TObject);
+begin
+  rbnBase64.Checked := False;
+  mmoSignature.Text := LbRSASSA1.Signature.IntStr;
 end;
 
 end.
